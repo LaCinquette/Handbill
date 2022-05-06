8cc893bde0582ed03395b2c44ae46b4a28833f93
Simon <yurik_sevastyanov123@mail.ru>
2022-05-07 01:19:50 +0300

    update

--- public/counter.js
+++ public/counter.js
@@ -2,5 +2,10 @@ const btn = document.getElementById("download_button");
 
 btn.addEventListener("click", () => {
     console.log("+1");
-    location.href = "/download";
+    // location.href = "/download";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/download");
+    // xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send();
 });
